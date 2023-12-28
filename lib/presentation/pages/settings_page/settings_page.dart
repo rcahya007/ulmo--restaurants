f8d399d757eeb5caf8d928c5da6bcd125cce6010
RendyCahyaE <rendycahya6@gmail.com>
2023-12-29 03:49:12 +0700

    add setting page

--- lib/presentation/pages/settings_page/settings_page.dart
+++ lib/presentation/pages/settings_page/settings_page.dart
@@ -13,21 +13,24 @@ class SettingsPage extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: [
-            ButtonTheme(
-              buttonColor: Colors.grey[300],
-              minWidth: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black87,
-                  foregroundColor: Colors.grey[300],
-                  minimumSize: const Size(88, 36),
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(2)),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: ButtonTheme(
+                buttonColor: Colors.grey[300],
+                minWidth: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black87,
+                    foregroundColor: Colors.grey[300],
+                    minimumSize: const Size(88, 36),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(2)),
+                    ),
                   ),
+                  onPressed: () {},
+                  child: const Text('Show Notification'),
                 ),
-                onPressed: () {},
-                child: const Text('Show Notification'),
               ),
             ),
           ],
